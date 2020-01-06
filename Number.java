7ca97b8e1a41e8ac80c11ebdbb5a2ff80344760d
LuthfieFauzan <lutfifauzan72@yahoo.com>
2020-01-06 21:37:41 +0700

    test

--- Number.java
+++ Number.java
@@ -2,7 +2,7 @@ public class Number {
     public static void main(String args[]) {
       System.out.println("Hello World");
         for (int i=0; i<100; i++){
-	if(i==2||i==3||i==5||i==7)
+	if(i==2||i==3||i==5)
 	System.out.println(i);
 	  if(!(i%2==0)){
 	    if(!(i%3==0)){
