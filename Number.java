745cf2ea04725eafa102d5bf815fe636460ab4e1
LuthfieFauzan <lutfifauzan72@yahoo.com>
2020-01-06 15:56:08 +0700

    second

--- Number.java
+++ Number.java
@@ -1,8 +1,16 @@
 public class Number {
     public static void main(String args[]) {
       System.out.println("Hello World");
-        for (int i=0; i<10; i++){
-            System.out.println(i);
+        for (int i=0; i<100; i++){
+	  if(!(i%2==0)){
+	    if(!(i%3==0)){
+		if(!(i%5==0)){
+			if(!(i%7==0)){
+		    System.out.println(i);
+		}
+	      }
+	    }
+	  }
         }
     }
 }
