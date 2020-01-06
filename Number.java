b8bac0b33c52daefb9aa04ba5837df323e29361b
LuthfieFauzan <lutfifauzan72@yahoo.com>
2020-01-06 16:08:54 +0700

    third

--- Number.java
+++ Number.java
@@ -2,6 +2,8 @@ public class Number {
     public static void main(String args[]) {
       System.out.println("Hello World");
         for (int i=0; i<100; i++){
+	if(i==2||i==3||i==5||i==7)
+	System.out.println(i);
 	  if(!(i%2==0)){
 	    if(!(i%3==0)){
 		if(!(i%5==0)){
